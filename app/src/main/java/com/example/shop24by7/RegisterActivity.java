ceb12b10112680da66c2ac13357994a784ce86ab
Sahitya77 <sahityanijhawan@gmail.com>
2021-09-08 17:30:39 +0530

    Sign up

--- app/src/main/java/com/example/shop24by7/RegisterActivity.java
+++ app/src/main/java/com/example/shop24by7/RegisterActivity.java
@@ -1,14 +1,29 @@
 package com.example.shop24by7;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        frameLayout = findViewById(R.id.register_framelayout);
+        setFragment(new SigninFragment());
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(frameLayout.getId(),fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/shop24by7/SigninFragment.java
+++ app/src/main/java/com/example/shop24by7/SigninFragment.java
@@ -2,11 +2,16 @@ package com.example.shop24by7;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +32,9 @@ public class SigninFragment extends Fragment {
     public SigninFragment() {
         // Required empty public constructor
     }
+
+    private TextView dontHaveAnAccount;
+    private FrameLayout parentFrameLayout;
 
     /**
      * Use this factory method to create a new instance of
@@ -59,6 +67,27 @@ public class SigninFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false);
+        View view = inflater.inflate(R.layout.fragment_signin, container, false);
+        dontHaveAnAccount = view.findViewById(R.id.dont_have_an_account);
+        parentFrameLayout = getActivity().findViewById(R.id.register_framelayout);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull  View view, @Nullable  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        dontHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new SignupFragment());
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_right,R.anim.slideout_from_left);
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.commit();
     }
 }
