ff62a67b395c906a570d8d3e9b680a69974b4609
Sahitya77 <sahityanijhawan@gmail.com>
2021-09-26 12:24:43 +0530

    categories

--- app/src/main/java/com/example/shop24by7/HomeFragment.java
+++ app/src/main/java/com/example/shop24by7/HomeFragment.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,6 +41,15 @@ public class HomeFragment extends Fragment {
 
     private RecyclerView categoryRecyclerView;
     private CategoryAdapter categoryAdapter;
+
+    private ViewPager bannerSliderViewPager;
+    private List<SliderModel> sliderModelList;
+
+    private int currentPage=2;
+
+    private Timer timer;
+    final private long DELAY_TIME=3000;
+    final private long PERIOD_TIME=3000;
 
     /**
      * Use this factory method to create a new instance of
@@ -86,6 +101,101 @@ public class HomeFragment extends Fragment {
         categoryAdapter= new CategoryAdapter(categoryModelList);
         categoryRecyclerView.setAdapter(categoryAdapter);
         categoryAdapter.notifyDataSetChanged();
+
+
+        bannerSliderViewPager=view.findViewById(R.id.banner_slider_view_pager);
+
+        sliderModelList = new ArrayList<SliderModel>();
+
+        sliderModelList.add(new SliderModel(R.mipmap.profile_placeholder));
+        sliderModelList.add(new SliderModel(R.mipmap.home_icon));
+
+        sliderModelList.add(new SliderModel(R.mipmap.green_email));
+        sliderModelList.add(new SliderModel(R.mipmap.red_email));
+        sliderModelList.add(new SliderModel(R.mipmap.wowoow));
+        sliderModelList.add(new SliderModel(R.mipmap.cart_white));
+        sliderModelList.add(new SliderModel(R.mipmap.profile_placeholder));
+        sliderModelList.add(new SliderModel(R.mipmap.home_icon));
+
+        sliderModelList.add(new SliderModel(R.mipmap.green_email));
+        sliderModelList.add(new SliderModel(R.mipmap.red_email));
+
+
+        SliderAdapter sliderAdapter = new SliderAdapter(sliderModelList);
+        bannerSliderViewPager.setAdapter(sliderAdapter);
+        bannerSliderViewPager.setClipToPadding(false);
+        bannerSliderViewPager.setPageMargin(20);
+
+        ViewPager.OnPageChangeListener onPageChangeListener =new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage=position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if(state==ViewPager.SCROLL_STATE_IDLE){
+                    pageLooper();
+                }
+            }
+        };
+        bannerSliderViewPager.addOnPageChangeListener(onPageChangeListener);
+
+        startBannerSlideShow();
+        bannerSliderViewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                pageLooper();
+                stopBannerSlideShow();
+                if(event.getAction()==MotionEvent.ACTION_UP){
+                    startBannerSlideShow();
+                }
+                return false;
+            }
+        });
         return view;
+
+
+    }
+    private void pageLooper(){
+        if(currentPage==sliderModelList.size()-2){
+            currentPage=2;
+            bannerSliderViewPager.setCurrentItem(currentPage,false);
+        }
+        if(currentPage==1){
+            currentPage=sliderModelList.size()-3;
+            bannerSliderViewPager.setCurrentItem(currentPage,false);
+        }
+    }
+
+    private void startBannerSlideShow(){
+        Handler handler = new Handler();
+        Runnable update =new Runnable(){
+            @Override
+            public void run() {
+                if(currentPage>=sliderModelList.size()){
+                    currentPage=1;
+
+                }
+                bannerSliderViewPager.setCurrentItem(currentPage++,true);
+            }
+        };
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        },DELAY_TIME,PERIOD_TIME );
+
+    }
+
+    private void stopBannerSlideShow(){
+        timer.cancel();
     }
 }
---
+++ app/src/main/java/com/example/shop24by7/SliderAdapter.java
@@ -0,0 +1,47 @@
+package com.example.shop24by7;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class SliderAdapter extends PagerAdapter {
+    private List<SliderModel> sliderModelList;
+
+    public SliderAdapter(List<SliderModel> sliderModelList) {
+        this.sliderModelList = sliderModelList;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Object instantiateItem(@NonNull @NotNull ViewGroup container, int position) {
+        View view= LayoutInflater.from(container.getContext()).inflate(R.layout.slider_layout,container,false);
+        ImageView banner = view.findViewById(R.id.banner_slide);
+        banner.setImageResource(sliderModelList.get(position).getBanner());
+        container.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull @NotNull View view, @NonNull @NotNull Object object) {
+        return view==object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull @NotNull ViewGroup container, int position, @NonNull @NotNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return sliderModelList.size();
+    }
+}
